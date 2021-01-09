a6a25b6a7d0ef539bbbd3b8314de0fd8c7edb0a5
bohdansok <73095219+bohdansok@users.noreply.github.com>
2021-01-09 13:04:31 +0200

    Update fr15_ukr.py

--- fr15_ukr.py
+++ fr15_ukr.py
@@ -1,6 +1,6 @@
 # first attemt to create a rec and comp app
 # Author - Bohdan SOKRUT
-# https://github.com/bohdansok/FR
+# https://github.com/bohdansok/Face_Recognition
 ##
     
 import face_recognition, os, pickle, os.path, glob
